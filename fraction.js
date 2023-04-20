e8ecd365c54741aaaee67d51dc3f61ef6456cfde
tejaswinise18 <115769533+tejaswinise18@users.noreply.github.com>
2023-04-20 11:44:31 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
 
+    def multiply(self, other):
+        numerator = self.numerator * other.numerator
+        denominator = self.denominator * other.denominator
+        return Fraction(numerator, denominator)
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
