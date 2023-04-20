50082148ff0d68f579ee650f07a7a4e5c2e58c74
IshitaSrivasavi <112959731+IshitaSrivasavi@users.noreply.github.com>
2023-04-20 11:41:12 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,16 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def subtract(self, other):
+        if isinstance(other, Fraction):
+            num = self.numerator * other.denominator - self.denominator * other.numerator
+            denom = self.denominator * other.denominator
+            return Fraction(num, denom)
+        else:
+            raise ValueError("Invalid operand for subtraction.")
 
