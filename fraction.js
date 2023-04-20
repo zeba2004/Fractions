41a6530a82534056df8d8347de06b0dc46f3bab9
saigawand <115694200+saigawand@users.noreply.github.com>
2023-04-20 11:38:45 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
 
+  add(other) {
+    const resultNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+    const resultDenominator = this.denominator * other.denominator;
+    return new Fraction(resultNumerator, resultDenominator);
+  }
+}
