1013c4228e763b0d16acde5c24ea61e693a0daae
IshitaSrivasavi <112959731+IshitaSrivasavi@users.noreply.github.com>
2023-04-20 11:53:38 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,22 @@
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+
+  static fromInput() {
+    const numerator = parseInt(prompt("Enter numerator:"));
+    const denominator = parseInt(prompt("Enter denominator:"));
+    return new Fraction(numerator, denominator);
+  }
+  subtract(other) {
+    const commonDenominator = this.denominator * other.denominator;
+    const numerator = this.numerator * other.denominator - other.numerator * this.denominator;
+    return new Fraction(numerator, commonDenominator);
+  }
+
+const fraction1 = Fraction.fromInput();
+const fraction2 = Fraction.fromInput();
+const difference = fraction1.subtract(fraction2);
+console.log(`Difference: ${difference.numerator}/${difference.denominator}`);
+
