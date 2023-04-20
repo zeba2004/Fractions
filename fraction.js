a1a90fd58d2fa7834ef78f2d84ea279a63426dda
saigawand <115694200+saigawand@users.noreply.github.com>
2023-04-20 11:57:56 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,12 +1,26 @@
-class Fraction {
-  constructor(numerator, denominator) {
-    this.numerator = numerator;
-    this.denominator = denominator;
-  }
+const prompt = require('prompt-sync')();
 
-  add(other) {
-    const resultNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
-    const resultDenominator = this.denominator * other.denominator;
-    return new Fraction(resultNumerator, resultDenominator);
+class Fraction {
+    constructor(numerator, denominator) {
+      this.numerator = numerator;
+      this.denominator = denominator;
+    }
+  
+    static fromInput() {
+      const numerator = parseInt(prompt("Enter numerator:"));
+      const denominator = parseInt(prompt("Enter denominator:"));
+      return new Fraction(numerator, denominator);
+    }
+  
+    add(other) {
+      const commonDenominator = this.denominator * other.denominator;
+      const numerator = this.numerator * other.denominator + other.numerator * this.denominator;
+      return new Fraction(numerator, commonDenominator);
+    }
   }
-}
+  
+  const fraction1 = Fraction.fromInput();
+  const fraction2 = Fraction.fromInput();
+  
+  const sum = fraction1.add(fraction2);
+  console.log(`Sum: ${sum.numerator}/${sum.denominator}`);
