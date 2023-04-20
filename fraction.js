5f5943dd65bbd46faa43daa5fd7bfdf3a16c55c4
zeba2004 <117371677+zeba2004@users.noreply.github.com>
2023-04-20 12:02:31 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,12 +1,25 @@
+const prompt=require('prompt-sync')();
 class Fraction {
   constructor(numerator, denominator) {
     this.numerator = numerator;
     this.denominator = denominator;
   }
 
-  add(other) {
-    const resultNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
-    const resultDenominator = this.denominator * other.denominator;
-    return new Fraction(resultNumerator, resultDenominator);
+  static fromInput() {
+    const numerator = parseInt(prompt("Enter numerator:"));
+    const denominator = parseInt(prompt("Enter denominator:"));
+    return new Fraction(numerator, denominator);
+  }
+
+  divide(other) {
+    const numerator = this.numerator * other.denominator;
+    const denominator = this.denominator * other.numerator;
+    return new Fraction(numerator, denominator);
   }
 }
+
+const fraction1 = Fraction.fromInput();
+const fraction2 = Fraction.fromInput();
+
+const quotient = fraction1.divide(fraction2);
+console.log(`Quotient: ${quotient.numerator}/${quotient.denominator}`);
