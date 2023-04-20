8dd0721369ccea299799e83f0eecab2caaa01ea4
zeba2004 <117371677+zeba2004@users.noreply.github.com>
2023-04-20 12:17:31 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,16 +1,23 @@
-class Fraction:
-    def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+const prompt=require('prompt-sync')();
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
 
-    def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
+  static fromInput() {
+    const numerator = parseInt(prompt("Enter numerator:"));
+    const denominator = parseInt(prompt("Enter denominator:"));
+    return new Fraction(numerator, denominator);
+  }
+  
+   subtract(other) {
+    const commonDenominator = this.denominator * other.denominator;
+    const numerator = this.numerator * other.denominator - other.numerator * this.denominator;
+    return new Fraction(numerator, commonDenominator);
+  }
+const fraction1 = Fraction.fromInput();
+const fraction2 = Fraction.fromInput();
 
-    def subtract(self, other):
-        if isinstance(other, Fraction):
-            num = self.numerator * other.denominator - self.denominator * other.numerator
-            denom = self.denominator * other.denominator
-            return Fraction(num, denom)
-        else:
-            raise ValueError("Invalid operand for subtraction.")
-
+const difference = fraction1.subtract(fraction2);
+console.log(`Difference: ${difference.numerator}/${difference.denominator}`);
